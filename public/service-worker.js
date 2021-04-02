2fcbec1d3c898bae3ca2622c9009a92a02dcc803
amcmilne <amcmilne@gmail.com>
2021-04-01 20:54:13 -0400

    1. fixed service worker bug 2. have a working indexedDB 3. move code from App.js 4. added IndexDB code to Newbids

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,56 +4,88 @@ console.log("Hi from your service-worker.js file!");
 
 var urlsToCache = [
   "/",
-  "osu-final-project/public/index.html",
-  "osu-final-project/public/js/index.js",
-  "osu-final-project/public/favicon.ico",
-  "osu-final-project/public/manifest.webmanifest",
+  "/index.html",
+  "/js/index.js",
+  "/favicon.ico",
+  "/manifest.webmanifest",
 ];
 
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-self.addEventListener("install", (evt) => {
-  evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/clients"))
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/newbids"))
   );
-});
-self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       console.log("Opened cache");
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
 });
-self.addEventListener("fetch", function (event) {
+
+// fetch
+self.addEventListener("fetch", (event) => {
+  // cache successful requests to the API
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(async (cache) => {
+          try {
+            const response = await fetch(event.request);
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          } catch (err) {
+            return await cache.match(event.request);
+          }
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-
-      return fetch(event.request).then(function (response) {
-        // Check if we received a valid response
-        if (
-          event.request.cache === "only-if-cached" &&
-          event.request.mode !== "same-origin"
-        ) {
-          return;
-        }
-
-        var responseToCache = response.clone();
-
-        caches.open(CACHE_NAME).then(function (cache) {
-          cache.put(event.request, responseToCache);
-        });
-
-        return response;
-      });
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
+
+// self.addEventListener("fetch", function (event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       // Cache hit - return response
+//       if (response) {
+//         return response;
+//       }
+
+//       return fetch(event.request).then(function (response) {
+//         // Check if we received a valid response
+//         if (
+//           event.request.cache === "only-if-cached" &&
+//           event.request.mode !== "same-origin"
+//         ) {
+//           return;
+//         }
+
+//         var responseToCache = response.clone();
+
+//         caches.open(CACHE_NAME).then(function (cache) {
+//           cache.put(event.request, responseToCache);
+//         });
+
+//         return response;
+//       });
+//     })
+//   );
+// });
 self.addEventListener("activate", function (event) {
   var cacheAllowlist = ["static-cache-v1", "data-cache-v1"];
 
--- src/components/Newbids.js
+++ src/components/Newbids.js
@@ -1,34 +1,121 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import "./CSS/theme.css"
+import Dexie from 'dexie';
+import { useLiveQuery } from "dexie-react-hooks";
 
+  //... create db
+  const db = new Dexie("bidDatabase");
+  db.version(1).stores({
+    clients: "++id, name, address, email",
+  })
 
-function Newbids() {
+const Newbids = () => {
+//... variable to store clients
+  const allClients = useLiveQuery(() => db.clients.toArray(), []);
+  if (!allClients) return null
+  
+  //... add client
+  const addClientToDb = async (event) => {
+    event.preventDefault()
+    const name = document.querySelector(".item-name").value;
+    const address = document.querySelector(".item-address").value;
+    const email = document.querySelector(".item-email").value;
+    await db.clients.add({
+      name,
+      address,
+      email,
+      clientBeenSaved: false,
+    })
+  }
+
+  // //... remove client
+  // const removeClientFromDb = async (id) => {
+  //   await db.items.delete(id);
+  // };
+
+  // //...update client
+  // const markAsSaved = async (id, event) => {
+  //   if (event.target.checked) {
+  //     await db.clients.update(id, { clientBeenSaved: true });
+  //   } else {
+  //     await db.clients.update(id, { clientBeenSaved: false });
+  //   }
+  // };
+
+  //...variable to hold client data
+  const clientData = allClients.map(
+    ({ id, name, address, email }) => (
+      <div className="row" key={id}>
+        <p className="col s3">
+          <label>
+            <span className="black-text">{name}</span>           
+          </label>
+        </p>
+        <p className="col s3">
+          <label>
+          <span className="black-text">{address}</span>         
+          </label>
+        </p>
+        <p className="col s3">
+          <label>
+          <span className="black-text">{email}</span>         
+          </label>
+        </p>
+      
+            
+      </div>
+    )
+  )
+
   return (
     <div>
       <div className="container">
-        <h1>Client Name</h1>
-        <input placeholder="John Doe" />
-        <br />
-        <br />
-        <h1>Address</h1>
-        <input placeholder="123 Main St. City, State 12345" />
-        <br />
-        <h1>Client Email</h1>
-        <input placeholder="user@email.com" />
-        <div>
-          <button>
-            {" "}
-            <Link to="/main"> Back</Link>
+        <h3 className="text center-align">New Bids</h3>
+        <form
+          className="add-item-form"
+          onSubmit={(event) => addClientToDb(event)}
+        >
+          <input
+            type="text"
+            className="item-name"
+            placeholder="Name"
+            required
+          />
+          <input
+            type="text"
+            className="item-address"
+            placeholder="Address"
+            required
+          />
+          <input
+            type="text"
+            className="item-email"
+            placeholder="Email"
+            required
+          />
+          <button type="submit" className="waves-effect waves-light btn right">
+            Add Client
           </button>
-          <button>
-            {" "}
-            <Link to="/paint"> Paint</Link>
-          </button>
-        </div>
+        </form>
+        {allClients.length > 0 && 
+          <div className="card">
+            <div className="card-content-round">
+              <form action="#">
+                {clientData}
+                </form>
+            </div>
+          </div>
+        }
+         <button>
+       <Link to="/main"> Back</Link>
+     </button>
+     <button>
+       <Link to="/paint"> Paint</Link>
+       </button>
       </div>
-    </div>
-  );
+      
+       </div>
+    )
 }
 
 export default Newbids;
