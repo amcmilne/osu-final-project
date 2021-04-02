3346b4db366886632df0de81271cfd663a6a2c13
Abdinajib Liban <abdinajibliban@gmail.com>
2021-04-02 00:33:26 -0400

    added scroll feature for existing bids, deleted settings button, rearranged h1, added main container

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,24 +4,31 @@ import { Link } from "react-router-dom";
 function Main() {
     return (
       <div>
-        <h1>Main</h1>
-        <div>
-          <button>setting</button>
-        </div>
-        <div>
-          <button>
-            <Link to="/newbids"> Add New Bid </Link>
-          </button>
-          <h1>Add Bids</h1>
-        </div>
-        <h3>Search existing Bids</h3>
-        <div>
-          <ul>
-            <li> Bid 1</li>
-            <li> Bid 2</li>
-            <li> Bid 3</li>
-            <li> Bid 4</li>
-          </ul>
+        <div className="container">
+          <h1>Main</h1>
+          <div>
+            <h2>Add New Bid</h2>
+            <button>
+              <Link to="/newbids"> Add Bid </Link>
+            </button>
+          </div>
+          <h3>Search existing Bids</h3>
+          <div className="myUl">
+            <ul >
+              <li> Bid 1</li>
+              <li> Bid 2</li>
+              <li> Bid 3</li>
+              <li> Bid 4</li>
+              <li> Bid 5</li>
+              <li> Bid 6</li>
+              <li> Bid 7</li>
+              <li> Bid 8</li>
+              <li> Bid 9</li>
+              <li> Bid 10</li>
+              <li> Bid 11</li>
+              <li> Bid 12</li>
+            </ul>
+          </div>
         </div>
       </div>
     );
