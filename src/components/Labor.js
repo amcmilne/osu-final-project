9dd0546990c15decba144e16bdbe4d0c3cfe8e8c
amcmilne <amcmilne@gmail.com>
2021-04-01 22:13:11 -0400

    indexedDB to newbids and labor components

--- src/components/Labor.js
+++ src/components/Labor.js
@@ -1,28 +1,83 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import "./CSS/theme.css"
+import Dexie from "dexie";
+import { useLiveQuery } from "dexie-react-hooks";
+import "./CSS/theme.css";
 
-function Labor() {
-    return (
+//... create db
+const db = new Dexie("laborDatabase");
+db.version(1).stores({
+  labor: "++id, painters, hours, notes",
+});
+
+const Labor = () => {
+  //... variable to store clients
+  const laborCost = useLiveQuery(() => db.labor.toArray(), []);
+  if (!laborCost) return null;
+
+  //... add labor
+  const addLaborToDb = async (event) => {
+    event.preventDefault();
+    const painters = document.querySelector(".item-painters").value;
+    const hours = document.querySelector(".item-hours").value;
+    const notes = document.querySelector(".item-notes").value;
+    await db.labor.add({
+      painters,
+      hours,
+      notes,
+    });
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <h1>Labor</h1>
+        <br />
+        <form
+          className="add-item-form"
+          onSubmit={(event) => addLaborToDb(event)}
+        >
+          <label for="NumberOfPainters"> Number of Painters </label>
+          <input
+            type="number"
+            className="item-painters"
+            placeholder="Number of Painters"
+          />
+          <br />
+          <label for="EstimatedHours"> Estimated Labor Hours </label>
+          <input
+            type="number"
+            className="item-hours"
+            placeholder="Number of Hours"
+          />
+          <br />
+          <br />
+          <br />
+          <h1>Labor Notes</h1>
+          <textarea
+            type="text"
+            cols="25"
+            rows="5"
+            className="item-notes"
+            placeholder="Add text here!!"
+          ></textarea>
+          <br />
+
+          <button type="submit" className="waves-effect waves-light btn right">
+            Add Labor
+          </button>
+        </form>
         <div>
-            <div className="container">
-
-            <h1>Labor</h1><br/>
-            <label for="NumberOfPainters"> Number of Painters </label>
-                 <input type="number"/><br/>
-             <label for="EstimatedHours"> Estimated Labor Hours </label>
-                 <input type="number"/><br/>
-                 <br/><br/>
-             <h1>Labor Notes</h1> 
-             <textarea name="text" cols="25" rows="5" placeholder="Add text here!!"></textarea> <br/>
-             <div>
-                 <button> <Link to="/materials"> Back</Link></button>
-                 <button> <Link to="/quote"> Quote</Link></button>
-             </div>
- 
-            </div>
+          <button>
+            <Link to="/materials"> Back</Link>
+          </button>
+          <button>
+            <Link to="/quote"> Quote</Link>
+          </button>
         </div>
-    )
- }
- 
- export default Labor;
+      </div>
+    </div>
+  );
+};
+
+export default Labor;
