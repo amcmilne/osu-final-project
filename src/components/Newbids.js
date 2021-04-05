2206a519aec0e8bfe5762d548b248666414b7527
amcmilne <amcmilne@gmail.com>
2021-04-05 15:03:25 -0400

    correction on Newbids

--- src/components/Newbids.js
+++ src/components/Newbids.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Dexie from "dexie";
 import { useLiveQuery } from "dexie-react-hooks";
-//import "./CSS/newbids.scss";
+import "./CSS/newbids.scss";
 
 //... create db
 const db = new Dexie("bidDatabase");
